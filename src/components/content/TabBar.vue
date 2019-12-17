d0138021896379e2e6ba59dd1f5cd95bbbb74db1
youguanxinqing <youguanxinqing@qq.com>
2019-12-17 23:16:10 +0800

    从 App.vue 中抽离 TabBar,TabBarItem

---
+++ src/components/content/TabBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="tab-bar">
+    <tab-bar>
+      <tab-bar-item path="/home" textColor="#d4237a">
+        <img slot="item-active" src="@/assets/img/tabbar/index-active.svg" alt="">
+        <img slot="item-deactive" src="@/assets/img/tabbar/index.svg">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" textColor="#d4237a">
+        <img slot="item-active" src="@/assets/img/tabbar/category-active.svg" alt="">
+        <img slot="item-deactive" src="@/assets/img/tabbar/category.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" textColor="#d4237a">
+        <img slot="item-active" src="@/assets/img/tabbar/cart-active.svg" alt="">
+        <img slot="item-deactive" src="@/assets/img/tabbar/cart.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" textColor="#d4237a">
+        <img slot="item-active" src="@/assets/img/tabbar/profile-active.svg" alt="">
+        <img slot="item-deactive" src="@/assets/img/tabbar/profile.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
+
+export default {
+    name: "TabBarUse",
+    components: {
+        TabBar,
+        TabBarItem
+    }
+}
+</script>
+
+<style>
+
+</style>
